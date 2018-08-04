a9336eed0bcf52f17b989903948e2003becc8d4a
rfarney3 <farneyr@yahoo.com>
2018-08-04 16:15:21 -0500

    rounded thumbnail pictures

--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,7 @@ const beginning = "https://image.tmdb.org/t/p/w185";
 
 const Movie = (props) => (
   <div className="movie-list">
-    <img alt={props.movie.poster_path} width="90px" src={`${beginning}` + props.movie.poster_path}/><br/>
+    <img alt={props.movie.poster_path} style={{ borderRadius: "10px" }} width="90px" src={`${beginning}` + props.movie.poster_path}/><br/>
     <span style={{ fontSize: "20px" }}><strong>{props.movie.title}</strong></span><br/>
     <strong>Released: </strong> {props.movie.release_date} <br />
     <strong>Popularity: </strong> {props.movie.popularity}
