d971501cad165b74697a153159ebb4465ea1dee6
rfarney3 <farneyr@yahoo.com>
2018-08-04 16:17:05 -0500

    underlined title

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ const beginning = "https://image.tmdb.org/t/p/w185";
 const Movie = (props) => (
   <div className="movie-list">
     <img alt={props.movie.poster_path} style={{ borderRadius: "10px" }} width="90px" src={`${beginning}` + props.movie.poster_path}/><br/>
-    <span style={{ fontSize: "20px" }}><strong>{props.movie.title}</strong></span><br/>
+    <span style={{ fontSize: "20px", textDecoration: "underline" }}><strong>{props.movie.title}</strong></span><br/>
     <strong>Released: </strong> {props.movie.release_date} <br />
     <strong>Popularity: </strong> {props.movie.popularity}
     <p style={{ fontStyle: "italic" }}>{props.movie.overview}</p>
